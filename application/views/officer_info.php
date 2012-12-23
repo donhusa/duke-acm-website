22440bb1da08f1c8bb9041ea06f48721a5a1dfb5
Don Husa <donhusa@gmail.com>
2012-12-23 11:16:45 -0500

    added contact pages

---
+++ application/views/officer_info.php
@@ -0,0 +1,16 @@
+<?php
+    $PresidentName = "Jimmy Mu";
+    $PresidentEmail = "jimmy.mu AT duke.edu";
+    
+    $VPName = "Don Husa";
+    $VPEmail = "dvh4 AT duke.edu";
+
+    $TreasName = "Eric Hu";
+    $TreasEmail = "eric.hu AT duke.edu";
+
+    $SecName = "Melissa Dalis";
+    $SecEmail = "melissa.dalis AT duke.edu";
+
+    $WebName = "Don Husa";
+    $WebEmail = "dvh4 AT duke.edu";
+?>
